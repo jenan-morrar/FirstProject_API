e35ff549b027bcb590654246a8320746ecbee27e
Jenan Morrar <jenanmorrar06@gmail.com>
2021-08-05 18:14:02 +0300

    Initial commit

---
+++ API_Project/tickets/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Movie, Guest, Reservation, Post
+
+admin.site.register(Movie)
+admin.site.register(Guest)
+admin.site.register(Reservation)
+admin.site.register(Post)
+
+#admin admin
